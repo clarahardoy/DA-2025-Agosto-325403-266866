8a0da246f8124eac526722c0c373ef7ef1546d3d
Clara Hardoy <clarahardoy@icloud.com>
2025-10-05 11:42:10 -0300

    inicial

---
+++ src/main/java/obg_sistema_pasajes/diseno/modelo/Vehiculo.java
@@ -0,0 +1,9 @@
+package obg_sistema_pasajes.diseno.modelo;
+
+public class Vehiculo {
+    private String matricula;
+    private String modelo;
+    private String color;
+    private CategoriaVehiculo categoria;
+
+}
