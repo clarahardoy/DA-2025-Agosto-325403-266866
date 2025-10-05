ec5df769367a07c70e86c6cce8948bf1a598ceb0
Clara Hardoy <clarahardoy@icloud.com>
2025-10-05 10:17:41 -0300

    initial commit

---
+++ src/test/java/obg_sistema_pasajes/diseno/DisenoApplicationTests.java
@@ -0,0 +1,13 @@
+package obg_sistema_pasajes.diseno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
